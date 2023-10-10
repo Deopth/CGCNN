bbe0597901e795610fd1c6b80a052d73b7c4e5d1
Deopth <42057336+Deopth@users.noreply.github.com>
2023-10-10 12:07:55 +0800

    interpreting model.py

--- cgcnn/model.py
+++ cgcnn/model.py
@@ -62,7 +62,7 @@ class ConvLayer(nn.Module):
         total_nbr_fea = torch.cat(
             [atom_in_fea.unsqueeze(1).expand(N, M, self.atom_fea_len),
              atom_nbr_fea, nbr_fea], dim=2)
-        total_gated_fea = self.fc_full(total_nbr_fea)
+        total_gated_fea = self.fc_full(total_nbr_fea)#total_nbr_fea中的类构造函数-初始化-- 全连接层
         total_gated_fea = self.bn1(total_gated_fea.view(
             -1, self.atom_fea_len*2)).view(N, M, self.atom_fea_len*2)
         nbr_filter, nbr_core = total_gated_fea.chunk(2, dim=2)
